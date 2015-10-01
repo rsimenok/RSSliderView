68f4a42600d71a9e05cc0fad03363e9eedfd9ab1
Roman Simenok <roma_simenok@mail.ru>
2015-10-01 13:55:10 +0300

    no message

--- RS_SliderView/ViewController.m
+++ RS_SliderView/ViewController.m
@@ -18,13 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    RS_SliderView *horSlider = [[RS_SliderView alloc] initWithFrame:CGRectMake(40, 40, 300, 80) andOrientation:Horizontal];
+    RS_SliderView *horSlider = [[RS_SliderView alloc] initWithFrame:CGRectMake(20, 40, 280, 70) andOrientation:Horizontal];
     horSlider.delegate = self;
     [horSlider setColorsForBackground:[UIColor colorWithRed:27.0/255.0 green:28.0/255.0 blue:37.0/255.0 alpha:1.0]
                            foreground:[UIColor colorWithRed:0.0 green:106.0/255.0 blue:95.0/255.0 alpha:1.0]
                                handle:[UIColor colorWithRed:0.0 green:205.0/255.0 blue:184.0/255.0 alpha:1.0]
                                border:[UIColor colorWithRed:0.0 green:205.0/255.0 blue:184.0/255.0 alpha:1.0]];
     horSlider.label.text = @"Horizontal slider";
+    // default font is Helvetica, size 24, so set font only if you need to change it.
     horSlider.label.font = [UIFont fontWithName:@"Helvetica" size:25];
     horSlider.label.textColor = [UIColor colorWithRed:0.0 green:205.0/255.0 blue:184.0/255.0 alpha:1.0];
     [self.view addSubview:horSlider];
@@ -39,10 +40,10 @@
     [vertSlider hideHandle];
     // if you don't like round corners or border
     [vertSlider removeRoundCorners:YES removeBorder:YES];
-    // Uncoment if you need text on you slider
-//    vertSlider.label.text = @"Vertical slider";
-//    vertSlider.label.font = [UIFont fontWithName:@"Helvetica" size:25];
-//    vertSlider.label.textColor = [UIColor colorWithRed:128.0/255.0 green:209.0/255.0 blue:79.0/255.0 alpha:1.0];
+    // Coment code below if you donn't need text on you slider
+    vertSlider.label.text = @"Vertical slider";
+    vertSlider.label.font = [UIFont fontWithName:@"Helvetica" size:25];
+    vertSlider.label.textColor = [UIColor colorWithRed:128.0/255.0 green:209.0/255.0 blue:79.0/255.0 alpha:1.0];
     [self.view addSubview:vertSlider];
 }
 
